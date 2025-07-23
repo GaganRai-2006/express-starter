b0c6091868d5899b37654a31322989bc8a6f5d51
gagan <raigagan084@gmail.com>
2025-07-23 13:33:10 +0530

    user schema added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ const serverConfig=require('./config/serverconfig');
 const connectDB = require('./config/dbconfig');
 const app=express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.text());
 
+app.post('/ping',(req,res)=>{
+    console.log(req.body);
+    return res.json({message:"pong"});
+})
 
 app.listen(serverConfig.PORT,async()=>{
      await connectDB()
---
+++ src/schema/userSchema.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        minlenght:[5,"firstname must be atleast 5 characters"],
+        maxlenght:[20,"firstname must be atmost 20 characters"],
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        minlenght:[5,"lasttname must be atleast 5 characters"],
+        maxlenght:[20,"lastname must be atmost 20 characters"],
+    },
+    mobileNumber:{
+        type:String,
+        required:true,
+        unique:[true,"mobile number must be unique"],
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:[true,"email should be provided"],
+        uninue:[true,"email must be unique"],
+        trim:true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password:{
+        type:String,
+        required:[true,"password should be provided"],
+        minlenght:[6,"password must be atleast 6 characters"]
+    }
+
+},{timestamps:true});
+const User=mongoose.model('User',userSchema);
+module.exports=User;
