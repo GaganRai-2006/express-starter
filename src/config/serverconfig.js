2289898879defe8fa4767501401e3615194f1a12
gagan <raigagan084@gmail.com>
2025-07-25 11:00:02 +0530

    added cloudinary config

--- src/config/serverconfig.js
+++ src/config/serverconfig.js
@@ -5,5 +5,8 @@ module.exports={
     PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
     SECRET_KEY:process.env.SECRET_KEY,
-    JWT_EXPIRY:process.env.JWT_EXPIRY
+    JWT_EXPIRY:process.env.JWT_EXPIRY,
+    CLOUDINARY_CLOUD_NAME:process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY:process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET:process.env.CLOUDINARY_API_SECRET
 }
